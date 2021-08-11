527281eaac3157f26143c0496d2587191778ce8a
honkwok2021 <87738305+honkwok2021@users.noreply.github.com>
2021-08-11 16:44:14 +1000

    Initial commit

---
+++ ContactsValidation.js
@@ -0,0 +1,126 @@
+﻿function validateNonEmpty(tf, helpText) { //tf=input to validate, helpText=display msg
+    //get the value from tf
+    var value = tf.value;
+
+    //check if value is empty
+    if (value) { //not empty
+        tf.className = "valid"; //change bgcolour to grey
+        helpText.innerHTML = ""; //reset the message
+        return true; //valid - not empty
+    }
+    else { //empty
+        tf.className = "invalid"; //change bgcolour to red
+        helpText.innerHTML = "Cannot be empty"; //error message
+        return false; //invalid - empty
+    }
+}
+
+function validateAll(form) {
+            //triggger the validation
+     var phoneNo = document.forms["Cform"]["phoneno"].value;
+     var email = document.forms["Cform"]["emailaddress"].value;
+     var name = document.forms["Cform"]["anyname"].value;
+     var message = document.forms["Cform"]["messagesss"].value;
+
+    if (!name || !message) {
+        
+        document.getElementById('formErrorMessage').innerText = "name and message, and either phone or email need to be filled in";
+        return false;
+    }
+
+    if (!phoneNo && !email) {
+        
+        document.getElementById('formErrorMessage').innerText = "either phone no or email need to be filled in";
+        return false;
+    }
+
+    //       if (name != "" && message != "" && phoneNo != "" && email != "") {
+    //           return success;
+    //       }
+    //       else if(name != "" && message != "" && phoneNo != "" || name != "" && message != "" && email != ""){
+    //           return success;
+    //       }
+    //       else if (name != "" && message != "" && phoneNo == "" && email == "") {
+               
+    //           helpText.innerHTML = "either phone no or email need to be filled in"
+    //           return false;
+    //       }
+    //else if (name == "" && message == "" && phoneNo == "" && email == "") {
+    //    helpText.innerHTML = "name and message need to either phone no or email need to be filled in"
+    //    return false;
+    //} 
+
+    document.getElementById('formErrorMessage').innerText = "";
+    return true;
+}
+
+function validatePhoneNo(tf, helpText) {
+    //check if the value is empty
+    var isNotEmpty = validateNonEmpty(tf, helpText);
+    if (isNotEmpty == false) {
+        return false;
+    }
+
+    //check if the value contains 4 digits
+    var value = tf.value;
+    var regex = /^61\d{9}$/;
+
+    var isDigits = regex.test(value);
+    if (isDigits) {
+        tf.className = "valid"; //change bgcolour to grey
+        helpText.innerHTML = ""; //reset the message
+        return true; //valid - 11 digits
+    }
+    else {
+        tf.className = "invalid"; //change bgcolour to red
+        helpText.innerHTML = "Can only be digit and should have 11 digit start with 61"; //error message
+        return false; //invalid - not 11 digits
+    }
+}
+function validateName(tf, helpText) {
+    //check if the value is empty
+    var isNotEmpty = validateNonEmpty(tf, helpText);
+    if (isNotEmpty == false) {
+        return false;
+    }
+
+    //check if the vlaue character
+    var value = tf.value;
+    var regex = /^[a-zA-Z]{2,}\w?/;
+
+    var atLeast2Letter = regex.test(value);
+    if (atLeast2Letter) {
+        tf.className = "valid";// change bgcolour to grey
+        helpText.innerHTML = ""; //reset the message
+        return true; //valid - at least 2 character
+    }
+    else {
+        tf.className = "invalid"; //change bgcolour to red
+        helpText.innerHTML = "minimum 2 letters and only between A-Z or a-z";//error message
+        return false;//invalid - not 2 letters
+    }
+}
+
+function validateEmail(tf, helpText) {
+    //check if the value is empty
+    var isNotEmpty = validateNonEmpty(tf, helpText);
+    if (isNotEmpty == false) {
+        return false;
+    }
+
+    //check if the value is valid for email
+    var value = tf.value;
+    var regex = /^[a-z0-9._%+-]{2,}@[a-z0-9.-]+\.[a-z]{2,4}$/;
+
+    var isDigits = regex.test(value);
+    if (isDigits) {
+        tf.className = "valid"; //change bgcolour to grey
+        helpText.innerHTML = ""; //reset the message
+        return true; //valid - email is valid
+    }
+    else {
+        tf.className = "invalid"; //change bgcolour to red
+        helpText.innerHTML = "not a valid email pattern e.g (at least 2 letters)@example.com"; //error message
+        return false; //invalid - not valid email pattern
+    }
+}
---
+++ PricingCalculator.js
@@ -0,0 +1,169 @@
+﻿/**Declare varible for date, firstDay and lastDay */
+//get the first day and last day of the current month
+var date = new Date(); //get the current date and time
+var firstDay = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+var lastDay = new Date(date.getFullYear(), date.getMonth() + 4, 0);
+
+/** use while loop to generate dates in the dropdown list with the next 3 months dates */
+while (firstDay <= lastDay) {
+    var option = document.createElement("option"); //<otpion></option>
+    var text = document.createTextNode(firstDay.toLocaleDateString());//XX/XX/XXXX
+    option.appendChild(text);//<otpion>XX/XX/XXXX</option>
+    document.getElementById("myDropDown").appendChild(option);
+    //<select id="myDropDown"><option>XX/XX/XXXX</option>..</select>
+    firstDay.setDate(firstDay.getDate() + 1);
+}
+/**delcare an array to store all selected dates*/
+var selectedDates = new Array(); //empty array
+
+
+/**
+ * define a function to display the selected dates and add the date into the list
+ * */
+function displayDate() {
+
+
+    /**stroe the selected date in to array*/
+    var selected = document.getElementById("myDropDown").value;
+    selectedDates.push(new DatePrice(selected));
+
+    /**display the total price of all selected dates*/
+    var total = 0;
+    for (var i = 0; i < selectedDates.length; i++) {
+        total += selectedDates[i].p;
+        
+    }
+    /**document.getElementById("demo0").innerHTML = "total price ";**/
+    document.getElementById("demo").innerHTML = total;
+
+    /** 
+     *  sort out the order od date
+     * @param {date} a - date A
+     * @param {date}b - date B
+     * @returns -1, 1 or 0
+     * */
+    selectedDates.sort(function (a, b) {
+        let selectDateA = a.d;
+        let selectDateB = b.d;
+        if (selectDateA < selectDateB) {
+            return -1;
+        }
+        if (selectDateA > selectDateB) {
+            return 1;
+        }
+        return 0;
+    });
+
+    /**add the slected date to the second drop down*/
+    document.getElementById("myDropDown2").innerHTML = "" //<select id="myDropDown2"</select> clear selection
+    for (var i = 0; i < selectedDates.length; i++) {
+        var option = document.createElement("option"); //<otpion></option>
+        var text = document.createTextNode(selectedDates[i].d.toLocaleDateString());//XX/XX/XXXX
+        option.appendChild(text);//<otpion>XX/XX/XXXX</option>
+        document.getElementById("myDropDown2").appendChild(option);
+    }
+}
+/**
+ * This is to find the sorted date and remove it with binary search*/
+function removeDate() {
+
+    /**
+     * To do a binary search of the date property
+     * @param {string} value 
+     * @param {number} list
+     * @returns {number} position of date property
+     */
+    function binarySearch(value, list) {
+
+        var match = value.match(/(\d{1,2})\/(\d{1,2})\/(\d{4})/);
+        let valueDate = new Date(parseInt(match[3]), parseInt(match[1]) - 1, parseInt(match[2]));
+
+        let first = 0;    //left endpoint
+        let last = list.length - 1;   //right endpoint
+        let position = -1;
+        let found = false;
+        let middle;
+
+        while (found === false && first <= last) {
+            middle = Math.floor((first + last) / 2);
+            if (list[middle].d.valueOf() == valueDate.valueOf()) {
+                found = true;
+                position = middle;
+            } else if (list[middle].d.valueOf() > valueDate.valueOf()) {  //if in lower half
+                last = middle - 1;
+            } else {  //in in upper half
+                first = middle + 1;
+            }
+        }
+        return position;
+    }
+
+    var selectedRemove = document.getElementById("myDropDown2").value;
+    let binaryPosition = binarySearch(selectedRemove, selectedDates);
+
+    /**remove the date with For loop */
+    for (var i = 0; i < selectedDates.length; i++) {
+
+        if (selectedRemove == selectedDates[i].d.toLocaleDateString()) {
+            //remove from second dropdown
+            //var indexDropDown = document.getElementById("myDropDown2").selectedIndex;
+            document.getElementById("myDropDown2").remove(binaryPosition);
+            //update total
+            var total = document.getElementById("demo").innerHTML;
+            total -= selectedDates[i].p;
+            //remove from array
+            selectedDates.splice(i, 1);
+        }
+    }
+    //update the total on demo
+    document.getElementById("demo").innerHTML = total;
+
+}
+
+
+
+
+/**
+ * define a class to store the bookingdate and booking price
+ */
+class DatePrice {
+    /**
+     * constructor to store date and get price
+     * @constructor
+     * @param {Date} d - a date added from the list
+     */
+    constructor(d) {//d - string booking date
+        this.d = new Date(d);//this.d (booking date) - Date obj property
+        this.p = this.getPrice(); // this.p (booking price) - a number property
+    }
+    /**
+     * Get the value of price with selected date using if and else if statement
+     * @param {Date} d - selected date
+     * @returns {number} the price value
+     */
+    getPrice(d) {
+
+
+        if (this.d.getMonth() === 0) {
+            return 250;
+        }
+        else if (this.d.getMonth() >= 1 && this.d.getMonth() <= 4) {
+            return 220;
+        }
+        else if (this.d.getMonth() >= 5 && this.d.getMonth() <= 7) {
+            return 200;
+        }
+        else if (this.d.getMonth() >= 8 && this.d.getMonth() <= 10) {
+            return 210
+        }
+        else if (this.d.getMonth() === 11 && this.d.getDate() <= 18) {
+            return 210;
+        }
+        else if (this.d.getMonth() === 11 && this.d.getDate() > 18) {
+            return 250;
+        }
+
+
+    }
+
+}
